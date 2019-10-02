7648b1d78f3b25e045ab5cd5709924a6b2948789
kruti <krutim@itpathsolutions.in>
2019-10-02 19:17:51 +0530

    Updated the color scheme for header and navbar links

--- resources/js/components/AdduserComponent.vue
+++ resources/js/components/AdduserComponent.vue
@@ -55,7 +55,7 @@
                             <p>{{ errors.password_confirmation[0] }}</p>
                         </div>
                     </div>
-                </div>
+                </div>                
                 <div class="form-group row mb-4">
                     <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3"></label>
                     <div class="col-sm-12 col-md-7">
